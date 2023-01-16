c0d3daefff60d5646c019a8e75f999c414dfb84c
Miguel Ibarra <40610736+migotron@users.noreply.github.com>
2023-01-16 15:27:32 -0600

    Initial commit 1) Create & Setup Project 2) Splash Screen 3) Main Screen 4) Connect Firebase

---
+++ app/src/main/java/com/emonics/bookappkotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.emonics.bookappkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.emonics.bookappkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // view binding
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // handle click, login
+        binding.loginBtn.setOnClickListener {
+            // will do later
+        }
+
+        //handle click, skip and continue to main screen
+        binding.skipBtn.setOnClickListener {
+            // will do later
+        }
+    }
+}
